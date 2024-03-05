a50e5e23da4b3d4be370d55276e109db5aacf5a9
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-03-05 09:51:31 -0300

    Criação de comandos de inicialização de Git

---
+++ 00-intro-git/comandos-git.sh
@@ -0,0 +1,9 @@
+git init # inicia um repositório git
+
+git add . # adiciona todos os arquivos
+
+git commit -m [Mensagem] # Comita as mudanças com a mensagem 
+
+git add remote [origin] [link] # Cria uma referência do link do repositório
+
+git push -u origin [branch] # Envia os commits para o repositório remoto (Necessário apenas pela primeira vez)
