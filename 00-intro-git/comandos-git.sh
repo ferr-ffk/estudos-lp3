3e3043e0ad22061842299dd1893dd0757987d786
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-03-05 09:54:39 -0300

    mend

--- 00-intro-git/comandos-git.sh
+++ 00-intro-git/comandos-git.sh
@@ -4,6 +4,6 @@ git add . # adiciona todos os arquivos
 
 git commit -m [Mensagem] # Comita as mudanças com a mensagem 
 
-git add remote [origin] [link] # Cria uma referência do link do repositório
+git remote [origin] add [link] # Cria uma referência do link do repositório
 
 git push -u origin [branch] # Envia os commits para o repositório remoto (Necessário apenas pela primeira vez)
