af31b25e5813586f8c9e5a04c1a381e08a9205ca
Fernando Freitas <126774900+ferr-ffk@users.noreply.github.com>
2024-04-10 11:26:37 -0300

    Create ex04.py

---
+++ 02-funcoes/ex04.py
@@ -0,0 +1,42 @@
+# Simulador de Eleições: Crie um programa que simule uma votação com três candidatos. O programa deve pedir ao usuário para votar várias vezes e, no final,
+# mostrar o número de votos de cada candidato e quem foi o vencedor. 
+
+
+def exibir_candidatos(cs: list):
+    for candidato in cs:
+        nome = candidato["nome"]
+        votos = candidato["votos"]
+
+        print(f"Candidato '{nome}' possui {votos}")
+
+
+def votar(candidatos, n: str):
+    for candidato in candidatos:
+        for key in candidato.keys():
+            if candidato[key] == n:
+                candidato["votos"] += 1
+
+
+def registrar_candidato(candidatos, nome: str):
+    candidatos.append({"nome": nome, "votos": 0})
+
+def main():
+    candidatos: list[dict] = []
+    
+    registrar_candidato(candidatos, "Joao Gomes")
+    registrar_candidato(candidatos, "Davi Gomes")
+    registrar_candidato(candidatos, "Rodrigo Gomes")
+
+    n = int(input("Digite quantas vezes deseja votar:"))
+
+    for _ in range(n):
+        exibir_candidatos(candidatos)
+        nome = input("Digite o nome do candidato que deseja votar:")
+
+        votar(candidatos, nome)
+
+    exibir_candidatos(candidatos)
+
+    # TODO: fazer ordenação do vencedor
+
+main()
