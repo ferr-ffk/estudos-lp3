d891207ed6e50a1e343271086e0ead0d35829e95
Fernando Freitas <freitax.li@gmail.com>
2024-04-10 22:23:09 -0300

    chore: Criação de método para ordenar os candidatos

--- 02-funcoes/exercicios/ex04.py
+++ 02-funcoes/exercicios/ex04.py
@@ -1,5 +1,5 @@
-# Simulador de Eleições: Crie um programa que simule uma votação com três candidatos. O programa deve pedir ao usuário para votar várias vezes e, no final,
-# mostrar o número de votos de cada candidato e quem foi o vencedor. 
+# Simulador de Eleições: Crie um programa que simule uma votação com três candidatos. O programa deve pedir ao usuário para votar várias vezes e, 
+# no final, mostrar o número de votos de cada candidato e quem foi o vencedor. 
 
 
 def exibir_candidatos(cs: list):
@@ -8,6 +8,18 @@ def exibir_candidatos(cs: list):
         votos = candidato["votos"]
 
         print(f"Candidato '{nome}' possui {votos}")
+        
+        
+def exibir_podio(cs: list):
+    posicao = 1
+    for candidato in cs:
+        print(f"{posicao}° colocado(a): {candidato["nome"]}, com {candidato["votos"]} votos")
+        
+        posicao += 1
+        
+    vencedor = cs.pop(0)
+        
+    print(f"\nVencedor: {vencedor["nome"]}!!")
 
 
 def votar(candidatos, n: str):
@@ -19,6 +31,11 @@ def votar(candidatos, n: str):
 
 def registrar_candidato(candidatos, nome: str):
     candidatos.append({"nome": nome, "votos": 0})
+    
+
+def ordenar_por_voto(candidato):
+    return candidato["votos"]
+
 
 def main():
     candidatos: list[dict] = []
@@ -35,8 +52,10 @@ def main():
 
         votar(candidatos, nome)
 
-    exibir_candidatos(candidatos)
-
-    # TODO: fazer ordenação do vencedor
+    candidatos.sort(key=ordenar_por_voto, reverse=True)
+    
+    print("Resultados:")
+    
+    exibir_podio(candidatos)
 
 main()
